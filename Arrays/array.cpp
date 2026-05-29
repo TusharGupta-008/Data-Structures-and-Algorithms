a69d3e8fa4b2ec6d9813ba5a94d45aaacbbd019b
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-05-30 02:52:24 +0530

    displaying temperature array

---
+++ Arrays/array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int temp[5] = {40, 50, 60, 70, 80};
+
+  for (int i = 0; i < 5; i++)
+  {
+    cout << "The tempratures are : " << temp[i];
+    cout << endl;
+  }
+}
