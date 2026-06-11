9b0e32ae328ba64519022f214fe50842ffbacf49
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-06-11 21:09:36 +0530

    next ques.

--- Arrays/array.cpp
+++ Arrays/array.cpp
@@ -18,27 +18,27 @@ using namespace std;
 //   return {};
 // }
 
-int removeDuplicates(vector<int> &arr)
-{
-  int n = arr.size();
-  int i = 1;
-  int j = 1;
+// int removeDuplicates(vector<int> &arr)
+// {
+//   int n = arr.size();
+//   int i = 1;
+//   int j = 1;
 
-  while (i < n)
-  {
-    if (arr[i] == arr[i - 1])
-    {
-      i++;
-    }
-    else
-    {
-      arr[j] = arr[i];
-      j++;
-      i++;
-    }
-  }
-  return j;
-}
+//   while (i < n)
+//   {
+//     if (arr[i] == arr[i - 1])
+//     {
+//       i++;
+//     }
+//     else
+//     {
+//       arr[j] = arr[i];
+//       j++;
+//       i++;
+//     }
+//   }
+//   return j;
+// }
 int main()
 {
   // int temp[5] = {40, 50, 60, 70, 80};
@@ -74,22 +74,22 @@ int main()
 
   // return 0;
 
-   int arr[5] = {10, 20, 50, 40, 30};
+  //  int arr[5] = {10, 20, 50, 40, 30};
 
-    int largest = arr[0];
-    int secondLargest = arr[0];
+  //   int largest = arr[0];
+  //   int secondLargest = arr[0];
 
-    for(int i = 1; i < 5; i++) {
-        if(arr[i] > largest) {
-            secondLargest = largest;
-            largest = arr[i];
-        }
-        else if(arr[i] > secondLargest && arr[i] != largest) {
-            secondLargest = arr[i];
-        }
-    }
+  //   for(int i = 1; i < 5; i++) {
+  //       if(arr[i] > largest) {
+  //           secondLargest = largest;
+  //           largest = arr[i];
+  //       }
+  //       else if(arr[i] > secondLargest && arr[i] != largest) {
+  //           secondLargest = arr[i];
+  //       }
+  //   }
 
-    cout << "Second Largest = " << secondLargest;
+  //   cout << "Second Largest = " << secondLargest;
 
-    return 0;
+  //   return 0;
 }
